6c16c81ed40c71964c26d7bb810e6d7162068b00
udayasree20 <udaysree811@gmail.com>
2026-09-24 09:33:29 +0530

    Add files via upload

---
+++ conditional operators.py
@@ -0,0 +1,28 @@
+print("hello class")
+age = int(input("enter a age:"))
+if(age>=18):
+    print("eligible for vote")
+    print("eligible for bike ride")
+else:
+    print("not eligible for vote")
+    print("not eligible for bike ride")
+print("enter your name")
+
+
+number=int(input("enter a number"))
+if(number==1):
+    print("MRDU")
+elif(number==2):
+    print("MBU")
+elif(number==3):
+    print("gitam")
+else:
+    print("Select another college")
+
+
+number=int(input("enter a number"))
+if(number>0):  
+    if(number<50):
+        print("number in between 1-50")
+    else:
+        print("number is greater than 50")
